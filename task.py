e35008c824ec5a2803ed9fa868d3fc5882418a62
saladin <saladinsayno@gmail.com>
2025-04-07 11:23:00 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,17 +1,6 @@
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+fruits = ["Apple", "Peach", "Pear"]
+for fruits in fruits:
+    print(fruits)
+    print(fruits + " pie")
 
-max_score = 0
-for score in student_scores:
-    if score > max_score:
-        max_score = score
-
-print(max_score)
-#max = sorted(student_scores)[-1]
-#print(max)
-
-
-
-
-#goal is to choose the largest number in the student score
-#print(range(1, 10))
-#print(max(student_scores))
+print(fruits)
