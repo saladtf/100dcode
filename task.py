482cffb5725165a7153159a14cd516dfc01b4e6e
saladin <saladinsayno@gmail.com>
2025-04-07 11:22:50 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,50 +1,17 @@
-from random import randint
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+print(max_score)
+#max = sorted(student_scores)[-1]
+#print(max)
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
 
-rockpapersciss= input("0 for rock 1 for paper 2 for scissors")
 
-if rockpapersciss == 0:
-    print (rock)
 
-elif rockpapersciss == 1:
-        print (paper)
-else:
-    print (scissors)
-
-computer_choice = randint (0 , 2)
-print("computer chose:")
-print([computer_choice])
-
-if computer_choice == 0:
-    print (rock)
-
-elif computer_choice == 1:
-        print (paper)
-else:
-    print (scissors)
+#goal is to choose the largest number in the student score
+#print(range(1, 10))
+#print(max(student_scores))
